df03952332a61351ad5df28db34c0544f2d39570
cjbrockmann <carlos.brockmann@gmx.de>
2022-01-17 15:58:04 +0100

    8 files modified, 14 deleted

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,25 +2,15 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './sites/home/home.component';
 import { LoremComponent } from './sites/lorem/lorem.component';
-import { WizardsComponent } from './sites/wizards/wizards.component';
-import { ZaehlerComponent } from './sites/zaehler/zaehler.component';
-import { GeraeteComponent } from './sites/geraete/geraete.component';
 
 const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full', ...{ title: 'Home' } },
   { path: 'home', component: HomeComponent, ...{ title: 'Home' } },
   {
     path: 'lorem',
     component: LoremComponent,
     ...{ title: 'Lorem', color: 'red' },
   },
-  { path: 'geraete', component: GeraeteComponent, ...{ title: 'Geräte' } },
-  { path: 'wizards', component: WizardsComponent, ...{ title: 'Wizards' } },
-  {
-    path: 'zaehler',
-    component: ZaehlerComponent,
-    ...{ title: 'Zählertabelle' },
-  },
-  { path: '', redirectTo: 'home', pathMatch: 'full', ...{ title: 'Home' } },
 
   // { path: 'book/:isbn', component: BookDetailsComponent },
   // { path: 'book', component: BookListComponent },
@@ -40,9 +30,5 @@ export class AppRoutingModule {
   public static navItems = [
     { path: 'home', title: 'Home' },
     { path: 'lorem', title: 'Lorem Ipsum' },
-    { path: 'home', title: 'Geräte' },
-    { path: 'wizards', title: 'Wizards' },
-    { path: 'home', title: 'Fernzugriff' },
-    { path: 'home', title: 'Zählertabellen' },
   ];
 }
